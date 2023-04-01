a57b50c3f39dba9a162cbe24fda377d5f995b5c6
marya belghith <62652076+marya563@users.noreply.github.com>
2023-04-01 17:42:47 +0100

    2nd commit
    fixed the remember me switch (using user defaults )/ made the singin navigate the user to it s profil data and added the ui for the navbar

---
+++ HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  hguesting
+//
+//  Created by Mac Mini 9 on 27/3/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ hguesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hguesting
+//
+//  Created by Mac Mini 9 on 20/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//
+
+
+    }
+    
+    }
